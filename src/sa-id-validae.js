c376cf70d897956269c427a095854bb96e7a0d96
Sphiwe Molala <molalasphiwe@gmail.com>
2016-09-03 21:57:09 +0200

    Ready for live testing

--- src/sa-id-validae.js
+++ src/sa-id-validae.js
@@ -57,293 +57,571 @@ if ( !document.getElementsByClassName ){
 
 
 	//Defination of the SA-ID-VALIDIFY class/object, properties will be added through the prototype object
-	w.SaIdValidae = function( idClassNameIdNumber ) {
+	w.SaIdValidae = function( idClassNameIdNumber , ignoreChars ) {
 
-		this.__construct( idClassNameIdNumber );
+		//will contain the value of the id number
+		this.idNumberObjects = [];
+
+		//will contain all captured errors as strings
+		this.errors = [];
+
+		//will contain all captured warnings as strings
+		this.warnings = [];
+
+		//will contain a string of charcode or range of charcodes to ignore during validation
+		this.ignoreChars = null;//by default numbers and
+
+		//Local settings of instantiated object
+		this.settings = {};
+
+		//theoretical exception classes ( just names ), for better presentation of error
+		this.exceptionClasses = ["Element Not Found Error :", "Time Not Set Error :"];
+
+		this.setUpIgnoreChars = function( charCodes ) {
+
+			if ( !charCodes )
+				return;
+
+			charCodes;
+
+			charCodes = charCodes.replace(/\s+|\]|\[/g,"");
+			var individualCharCodes = charCodes.split(":");
+			this.ignoreChars = [];
+
+			for ( var i = 0; i < individualCharCodes.length; ++i ) {
+				
+				var invalid = false;
+				var rangeValues = individualCharCodes[i].split("-");
+				this.ignoreChars.push( rangeValues );
+
+				if (  rangeValues.length > 2 ){
+
+					invalid = true;
+				
+				} else {
+
+					if ( !/^\d+$/.test( rangeValues[0] ) ) {
+						
+						invalid = true;
+
+					}
+
+					if ( 2 == rangeValues.length ) {
+
+						if ( !/^\d+$/.test( rangeValues[1] ) ) {
+
+							invalid = true;
+
+						} else if ( !invalid ) {
+
+							if ( rangeValues[0] > rangeValues[1] ) {
+
+								invalid = true;
+
+							}
+
+						}
+
+					}
+
+				}
+
+				console.log( this.ignoreChars );
+
+				if ( invalid ) {
+
+					this.ignoreChars = null;
+					return false;
+				}
+			}
+		}
+
+		this.escapeIfRegExpChar = function( regChar ) {
+
+			var regChars = ["]","[","^","$",".","|","?","*","+","(",")","{","}"];
+
+			for ( var i = 0; i < regChars.length; ++i ) {
+
+				if ( regChars[i] == regChar ) {
+
+					return "\\" + regChar;
+
+				}
+
+			}
+
+			return regChar;
+			
+		}
+
+		this.removeIgnoreChars = function( idNumber ) {
+
+
+			if ( !this.ignoreChars ) {
+
+				return idNumber;
+
+			}
+
+			for ( var i = 0; i < this.ignoreChars.length; ++i ) {
+
+				if ( this.ignoreChars[i].length == 2 ) {
+
+					for ( var r = this.ignoreChars[i][0] ; r <= this.ignoreChars[i][1] ; ++r ) {
+						
+						idNumber = idNumber.replace( new RegExp( ( r == 92 ? "\\" : this.escapeIfRegExpChar( String.fromCharCode( r ) ) ), "g" ), "" );
+					
+					}
+
+				} else {
+					
+					idNumber = idNumber.replace( new RegExp( ( r == 92 ? "\\" : this.escapeIfRegExpChar( String.fromCharCode( this.ignoreChars[i][0] ) ) ) , "g"), "" );
+
+				}
+
+			}
+
+			return idNumber;
+
+		}
+		this.createLocalSettings = function() {
+
+			this.settings = this.copyFrom( this.w.SaIdValidae.settings );
+
+		}
+
+		this.copyFrom = function( source, excludeFunctions ){
+
+			var copy = new Object();
+			for ( prop  in source ) {
+
+				if ( excludeFunctions ) {
+
+					if ( typeof source[prop] == 'function' )
+
+						continue;
+
+				}
+
+				if ( source[prop] instanceof Array ){
+
+					var array = source[prop];
+					for ( var i = 0; i < array.length; ++i ) {
+
+						if ( array[i] instanceof Object ) {
+
+							array[i] = this.copyFrom( array[i] );
+
+						}
+
+					}
+
+					copy[prop] = [].concat( array );
+
+				}
+				else if ( source[prop] instanceof Object )
+					copy[prop] = this.copyFrom( source[prop] );
+				else
+					copy[prop] = source[prop];
+			}
+			return copy;
+
+		}
+
+		//Define a custom constructor for our class/object
+		//RETURNS void
+		this.__construct = function( idClassNameIdNumber, ignoreChars ) {
+
+			this.setUpIgnoreChars( ignoreChars );
+			this.createLocalSettings();
+
+			var noElementFoundError = "'" + idClassNameIdNumber + "' does not match any element!";
+
+			if ( /^\..*/.test( idClassNameIdNumber ) ){//if className was sent
+
+				var elements = document.getElementsByClassName( idClassNameIdNumber.replace(/^\./,"") );
+
+				for( var i = 0; i < elements.length ; ++i ) {
+
+					this.idNumberObjects.push({ elem:elements[i], value:"" });
+
+				}
+
+				if ( !elements ){
+
+					this.logError( noElementFoundError, 0 );
+
+					return;
+				
+				} else if ( !elements.length ) {
+
+					this.logError( noElementFoundError, 0 );
+
+					return;
+
+				}
+
+			} else if ( /^#.*/.test( idClassNameIdNumber ) ) {//if id was sent
+
+				var element = document.getElementById( idClassNameIdNumber.replace(/^#/,"") );
+
+				if ( !element ) {
+
+					this.logError( noElementFoundError, 0 );
+
+					return;
+
+				} else {
+
+					this.idNumberObjects.push( { elem:element, value:"" } );
+
+				}
+
+			} else {
+
+				this.idNumberObjects.push({ elem:null,value: idClassNameIdNumber});
+
+			}
+
+			this.setUpIdNumberValues();
+
+
+			if ( this.settings.considerLeapYear && !this.settings.year )
+
+				this.logWarning( "Requested consideration of Leap Year but did not set Year in settings!", 1 );
+
+			//console.log( this.idNumberObjects )
+
+		}
+
+		this.validate = function() {
+			var results = [];
+
+
+
+			if ( !this.settings.secondLastDigitAcceptables instanceof Array ){
+
+				this.logWarning( "Customly defined secondLastDigitAcceptables value will be ignored since not Array");
+				this.settings.secondLastDigitAcceptables = [ 8 , 9 ];
+
+			}
+
+			for( var i = 0; i < this.idNumberObjects.length; ++i ) {
+
+				var idNumber = this.idNumberObjects[i].value, valid = false, messages = [], gender = null,citizen=null;
+				if ( this.settings.ignoreWhiteSpace ){
+					idNumber = idNumber.replace(/\s*/g,"");
+				}
+				
+				idNumber = this.removeIgnoreChars( idNumber );
+				
+
+				if ( idNumber.length != 13 )
+
+					messages.push( this.settings.messages.invalidLength );
+				
+				else if ( !/^\d+$/.test( idNumber ) ) {
+
+					messages.push( this.settings.messages.invalidCharacters );
+
+				} else{
+
+					var y = idNumber.substring(0,2), m = idNumber.substring(2,4), d = idNumber.substring(4,6), g = idNumber.substring(6,7), c = idNumber.substring(10,11), the8 = idNumber.substring(11,12);
+
+					if ( g < 5 )
+
+						gender = "f";
+
+					else
+
+						gender = "m";					
+
+
+					if (  c == 1 )
+
+						citizen = "SA Citizen";
+
+					else if ( c == 0 )
+
+						citizen = "Non-SA Citizen";
+
+					else {
+
+						messages.push( this.settings.messages.invalidCitizenNumber );
+
+					}
+
+					var the8IsOk = false;
+					for ( var z = 0; z < this.settings.secondLastDigitAcceptables.length; ++z ) {
+						
+						if ( this.settings.secondLastDigitAcceptables[z] == the8 ){
+
+							the8IsOk = true;
+							break;
+						
+						}	
+
+					}
+
+					if ( !the8IsOk ) {
+
+						messages.push( this.settings.messages.invalid8 );
+
+					}
+
+					//validate in order of appearance
+					if ( d > 31 || d == 0 ) {
+
+						messages.push( this.settings.messages.invalidDay );
+
+					}
+
+					if ( !( m > 12 || m == 0 ) ) {
+
+						if ( !(d > 31 || d == 0) ){
+
+							if ( !this.isDayInmonth( parseInt( d ), parseInt( m ) ) ) {//note tests for months ending on the 31st will always be true
+
+								messages.push( this.settings.messages.invalidDay );
+
+							}
+
+						}
+
+					} else {
+
+						messages.push( this.settings.messages.invalidMonth );
+					}
+				}
+
+				if ( !messages.length )
+
+					valid = true;
+
+				results.push( { valid: valid , messages: messages, gender:gender, citizen:citizen } );
+			}
+
+			return results;
+
+		}
+
+		this.isDayInmonth = function( day, month ) {
+			var lastDays = [ null , 31, 28, 31, 30, 31, 30, 31 ,31 ,30, 31, 30, 31];//indexes represent months numbers, 1 = January and so on
+
+			if ( lastDays[ month ] < day ){
+
+				if ( month == 2 ) {
+
+					if ( this.settings.considerLeapYear ) {
+
+						if ( /^\d{4}$/.test( this.settings.year + "" ) ) {
+
+							if ( this.settings.year%4 == 0 ) {
+
+								if ( 29 < day )
+
+									return false;
+
+								else
+
+									return true;
+
+							}
+
+						} else {
+
+							this.logWarning( "Leap Year consideration aborted due to invalid year value in settings" );
+							this.settings.considerLeapYear = false;
+
+						}
+
+					}
+
+				}
+
+				return false;
+
+			}
+				
+			return true;
+		}
+
+		this.setUpIdNumberValues = function() {
+
+			for( var i = 0; i < this.idNumberObjects.length; ++i ) {
+
+				if (this.idNumberObjects[i].elem){
+					if ( !this.idNumberObjects[i].elem.value ){
+
+						this.idNumberObjects[i].value = this.idNumberObjects[i].elem.innerHTML;
+
+					} else {
+
+						this.idNumberObjects[i].value = this.idNumberObjects[i].elem.value;
+
+					}
+				}
+				
+			}
+
+		}
+
+		this.logError = function ( err, whichError ) {
+
+			err = this.exceptionClasses[ whichError ] + err;
+			p.errors.push( err );
+
+			if ( this.settings.showCapturedErrors )
+				console.error( err );
+
+		}
+
+		this.logWarning = function ( warng ) {
+
+			this.warnings.push( warng );
+			if ( this.settings.showCapturedWarnings )
+				console.warn( warng );
+
+		}
+
+		this.__construct( idClassNameIdNumber , ignoreChars );
 
 	}
-	w.W = function(){this.fruits = "apples";}
 
 	w.SaIdValidae.settings = {
 		showCapturedErrors:true,
 		showCapturedWarnings:true,
 		ignoreWhiteSpace:true,
 		messages : {
+			invalidCharacters:"Invalid characters in id number",
 			invalidLength:"Invalid length",
 			invalidDay:"Invalid day",
 			invalidMonth:"Invalid Month",
 			invalidYear:"Invalid Year",
+			invalidCitizenNumber:"Invalid Citizen Number",
+			invalid8:"Invalid 8"
 		},
-		considerLeapYear:true,
-		year:2016,
-		ignoreByCharCode:false
+		considerLeapYear:false,
+		year:null,
+		ignoreByCharCode:false,
+		secondLastDigitAcceptables:[8,9]
 	}
 
 	//lets store reference to the prototype object for easy access
 	p = w.SaIdValidae.prototype;
-
 	//Store reference of the window and document object for easy access later
-	p.w = w;
-	p.d = d;
+	w.SaIdValidae.prototype.w = w;
+	w.SaIdValidae.prototype.d = d;
 
-	//will contain the value of the id number
-	p.idNumberObjects = [];
+	w.SaIdValidae.repeatString = function( string, factor ) {
 
-	//will contain all captured errors as strings
-	p.errors = [];
+		if ( !string )
 
-	//will contain all captured warnings as strings
-	p.warnings = [];
+			string = "";
 
-	//will contain a string of charcode or range of charcodes to ignore during validation
-	p.ignoreChars = null;//by default numbers and
+		if ( factor < 1 ){
 
-	//Local settings of instantiated object
-	p.settings = {};
-
-	//theoretical exception classes ( just names ), for better presentation of error
-	p.exceptionClasses = ["Element Not Found Error :", "Time Not Set Error :"];
-
-	p.setUpIgnoreChars = function( charCodes ) {
-
-		if ( !charCodes )
-			return;
-
-		charCodes;
-
-		charCodes = charCodes.replace(/\s+|\]|\[/g,"");
-		var individualCharCodes = charCodes.split(":");
-		for ( var i = 0; i < individualCharCodes.length; ++i ) {
-			
-			var rangeValues = individualCharCodes[i].split("-");
-			
-			if ( !/^\d+$/.test( rangeValues ) ){
-
-				p.ignoreChars = null;
-				return false;
-			
-			}
+			p.logError( "repeatString( string, factor ) expects integer factor of 1 or greater" );
+			return string;
+	
 		}
-	}
 
-	p.createLocalSettings = function() {
+		var newString = "";
 
-		this.settings = this.copyFrom( this.w.SaIdValidae.settings );
+		for ( var e = 0;e < factor; ++e ) {
 
-	}
+			newString += string;
 
-	p.copyFrom = function( source ){
-
-		var copy = new Object();
-		for ( prop  in source ) {
-
-			if ( source[prop] instanceof Object )
-				copy[prop] = this.copyFrom( source[prop] );
-			else
-				copy[prop] = source[prop];
 		}
-		return copy;
-		
+
+		return newString;
+
 	}
 
-	//Define a custom constructor for our class/object
-	//RETURNS void
-	p.__construct = function( idClassNameIdNumber, ignoreChars ) {
+	w.SaIdValidae.toHTMLString = function( object, withoutDelimeters, objectLevel ) {
 
-		this.setUpIgnoreChars( ignoreChars );
-		this.createLocalSettings();
+		return SaIdValidae.__toString( object, withoutDelimeters, objectLevel ).replace(/\n/g,"<br>").replace(/\t/g,"&nbsp;&nbsp;&nbsp;&nbsp;");
 
-		var noElementFoundError = "'" + idClassNameIdNumber + "' does not match any element!";
+	}
 
-		if ( /^\..*/.test( idClassNameIdNumber ) ){//if className was sent
+	w.SaIdValidae.__toString = function( object, withoutDelimeters , objectLevel ) {
 
-			var elements = document.getElementsByClassName( idClassNameIdNumber.replace(/^\./,"") );
+		if ( !object ){
 
-			for( var i = 0; i < elements.length ; ++i ) {
+			return "";
 
-				this.idNumberObjects.push({ elem:elements[i], value:"" });
+		}
 
-			}
+		if ( object == window || object == document ) {
 
-			if ( !elements ){
+			return "{" + object.toString() + "}";
 
-				this.logError( noElementFoundError, 0 );
+		}
 
-				return;
-			
-			} else if ( !elements.length ) {
+		if ( window.HTMLElement !== undefined ){
 
-				this.logError( noElementFoundError, 0 );
+			if ( object instanceof HTMLElement ) {
 
-				return;
-
-			}
-
-		} else if ( /^#.*/.test( idClassNameIdNumber ) ) {//if id was sent
-
-			var element = document.getElementById( idClassNameIdNumber.replace(/^#/,"") );
-
-			if ( !element ) {
-
-				this.logError( noElementFoundError, 0 );
-
-				return;
-
-			} else {
-
-				this.idNumberObjects.push( { elem:element, value:"" } );
+				return "{" + object.toString() + "}";
 
 			}
 
 		} else {
 
-			this.idNumberObjects.push({ elem:null,value: idClassNameIdNumber});
+				if ( (typeof obj==="object") && (obj.nodeType===1) && (typeof obj.style === "object") && (typeof obj.ownerDocument ==="object") )
+
+					return "{" + object.toString() + "}";
 
 		}
 
-		this.setUpIdNumberValues();
+		if ( !objectLevel )
 
+			objectLevel = 1;
 
-		if ( this.settings.considerLeapYear && !this.settings.year )
+		var string = "";
 
-			this.logWarning( "Requested consideration of Leap Year but did not set Year in settings!", 1 );
+		var indentation = SaIdValidae.repeatString( "\t", objectLevel );//indentation for 'object'
 
-	}
+		var innerIndentation = indentation + "\t";//indentation for properties of 'object'
 
-	p.validate = function() {
-		var results = [];
-		for( var i = 0; i < this.idNumberObjects.length; ++i ) {
+		var openingDelimeter = "{", closingDelimeter = "}", propValueSeparator = " : ", class_ = "Object ";//Delimeters and className
 
-			var idNumber = this.idNumberObjects[i].value, valid = false, messages = [], gender = null;
-			if ( this.ignoreWhiteSpace ){
-				idNumber = idNumber.replace(/\s*/g,"");
-			}
+		if ( object instanceof Array ){
 
-			if ( idNumber.length != 13 )
-				messages.push( this.settings.messages.invalidLength );
-			else{
-
-				var y = idNumber.substring(0,2), m = idNumber.substring(2,4), d = idNumber.substring(4,6), g = idNumber.substring(6,7);
-
-				if ( g < 5 )
-
-					gender = "f";
-
-				else
-
-					gender = "m";					
-
-				//validate in order of appearance
-				if ( d > 31 || d == 0 ) {
-
-					messages.push( this.settings.messages.invalidDay );
-
-				}
-
-				if ( !( m > 12 || m == 0 ) ) {
-
-					if ( !(d > 31 || d == 0) ){
-
-						if ( !this.isDayInmonth( parseInt( d ), parseInt( m ) ) ) {//note tests for months ending on the 31st will always be true
-
-							messages.push( this.settings.messages.invalidDay );
-
-						}
-
-					}
-
-				} else {
-
-					messages.push( this.settings.messages.invalidMonth );
-				}
-			}
-
-			if ( !messages.length )
-
-				valid = true;
-
-			results.push( { valid: valid , messages: messages, gender:gender } );
-		}
-
-		return results;
-
-	}
-
-	p.isDayInmonth = function( day, month ) {
-		var lastDays = [ null , 31, 28, 31, 30, 31, 30, 31 ,31 ,30, 31, 30, 31];//indexes represent months numbers, 1 = January and so on
-
-		if ( lastDays[ month ] < day ){
-
-			if ( month == 2 ) {
-
-				if ( this.settings.considerLeapYear ) {
-
-					if ( /^\d{4}$/.test( this.settings.year + "" ) ) {
-						if ( this.settings.year%4 == 0 ) {
-
-							if ( 29 < day )
-
-								return false;
-
-							else
-
-								return true;
-
-						}
-
-					} else {
-
-						this.logWarning( "Leap Year consideration aborted due to invalid year value in settings" );
-						this.settings.considerLeapYear = false;
-
-					}
-
-				}
-
-			}
-
-			return false;
+			openingDelimeter = "["; closingDelimeter = "]"; propValueSeparator = " => "; class_ = "Array ";//Delimeters and className
 
 		}
+
+		if ( withoutDelimeters ) {
+
+			openingDelimeter = ""; closingDelimeter = ""; class_ = "";
+
+		}
+
+		string = ( objectLevel == 1 ? "\n" + indentation : "" ) + ( object instanceof Array ? "" :  class_  ) + openingDelimeter;
+		
+		for ( prop in object ) {
+
+			if ( typeof object[ prop ] == "function" ){
+
+				string += "\n" + innerIndentation + prop  + propValueSeparator + "function() { [Some-Code] }";
+
+			} else if ( object[ prop ] instanceof Object ){
+						
+				string += "\n" + innerIndentation + prop + propValueSeparator + SaIdValidae.__toString( object[ prop ], withoutDelimeters, ( objectLevel + 1 ) );		
 			
-		return true;
-	}
+			}  else {
 
-	p.setUpIdNumberValues = function() {
+				string += "\n" + innerIndentation + prop + propValueSeparator + object[ prop ];
 
-		for( var i = 0; i < this.idNumberObjects.length; ++i ) {
-
-			if (this.idNumberObjects[i].elem){
-				if ( !this.idNumberObjects[i].elem.value ){
-
-					this.idNumberObjects[i].value = this.idNumberObjects[i].elem.innerHTML;
-
-				} else {
-
-					this.idNumberObjects[i].value = this.idNumberObjects[i].elem.value;
-
-				}
 			}
-			
+
 		}
 
-	}
+		if ( closingDelimeter.length )
+			string += "\n" + indentation + closingDelimeter;
 
-	p.logError = function ( err, whichError ) {
-
-		err = this.exceptionClasses[ whichError ] + err;
-		p.errors.push( err );
-
-		if ( this.settings.showCapturedErrors )
-			console.error( err );
-
-	}
-
-	p.logWarning = function ( warng ) {
-
-		this.warnings.push( warng );
-		if ( this.settings.showCapturedWarnings )
-			console.warn( warng );
+		return string;
 
 	}
 
